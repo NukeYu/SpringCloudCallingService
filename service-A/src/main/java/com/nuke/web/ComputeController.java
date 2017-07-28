0f16d1c942f77c3897a837d52b82a042b6a8dbf4
yuqiang@cn.ibm.com <yuqiang@cn.ibm.com>
2017-07-28 15:11:29 +0800

    adding maven control for the 2 services

--- service-A/src/main/java/com/nuke/web/ComputeController.java
+++ service-A/src/main/java/com/nuke/web/ComputeController.java
@@ -1,4 +1,4 @@
-package com.didispace.web;
+package com.nuke.web;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +22,7 @@ public class ComputeController {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-        return "From Service-B, Result is " + r;
+        return "From Service-A, Result is " + r;
     }
 
 }
--- service-B/src/main/java/com/nuke/ComputeServiceApplication.java
+++ service-B/src/main/java/com/nuke/ComputeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.didispace;
+package com.nuke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
